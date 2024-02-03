e176f376cea09d9be20d3b51ac441ae8028217f5
alexcuadroo <com.alexcuadro@gmail.com>
2024-02-03 18:39:08 -0300

    se colocan los datos en un js a parte para mejor mantenibilidad

---
+++ data.js
@@ -0,0 +1,15 @@
+export const pages = [
+            { url: 'lenguajes-y-sistemas-formales.html', title: 'Lenguajes y sistemas formales' },
+            { url: 'conjuntos-inductivos.html', title: 'Conjuntos inductivos' },
+            { url: 'induccion-primitiva.html', title: 'Inducción primitiva' },
+            { url: 'induccion-primitiva.html', title: 'Funciones recursivas primitivas' },
+            { url: 'induccion-primitiva.html', title: 'Definiciones inductivas libres y no libres' },
+            { url: 'induccion-primitiva.html', title: 'Lógica Proposicional Sintaxis' },
+            { url: 'induccion-primitiva.html', title: 'Lógica Proposicional Semántica' },
+            { url: 'induccion-primitiva.html', title: 'Deduccion Natural en Proposicional' },
+            { url: 'induccion-primitiva.html', title: 'Sintaxis de la Lógica de Predicados' },
+            { url: 'induccion-primitiva.html', title: 'Semántica de la Lógica de Predicados' },
+            { url: 'induccion-primitiva.html', title: 'Propiedades de la Lógica de Predicados' },
+            { url: 'induccion-primitiva.html', title: 'Otros' },
+  ];
+  
