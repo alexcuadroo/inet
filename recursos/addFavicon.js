c947948513893b5c09166b16540f49f2944a595f
alexcuadroo <com.alexcuadro@gmail.com>
2024-06-08 19:31:44 -0300

    favicons

---
+++ recursos/addFavicon.js
@@ -0,0 +1,7 @@
+(function() {
+    var link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = '/recursos/fav_edu.webp';
+    link.type = 'image/webp';
+    document.head.appendChild(link);
+})();
